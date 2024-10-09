3d290c52a33bbb11f2bf8a6a7da3da8e506c0ab6
Tashi Wangchuk <tashiwangchuk619@gmail.com>
2024-10-09 23:50:26 +0600

    add credentialProvider

--- src/auth.js
+++ src/auth.js
@@ -1,19 +1,49 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
- 
-export const { handlers:{GET,POST}, auth, signIn, signOut } = NextAuth({
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { getUserByEmail } from "./data/users";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
   providers: [
     GoogleProvider({
-        clientId:process.env.GOOGLE_CLIENT_ID,
-        clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-        authorization: {
-            params: {
-              prompt: "consent",
-              access_type: "offline",
-              response_type: "code",
-            },
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+    CredentialsProvider({
+      async authorize(credentials) {
+        if (credentials === null) return null;
+        try {
+          const user = getUserByEmail(credentials?.email);
+
+          if (user) {
+            const isMatch = user?.password === credentials?.password;
+            if (isMatch) {
+              return user
+            }else{
+              throw new Error("Check your password")
+            }
+          } else {
+            throw new Error("User not found");
+          }
+        } catch (error) {
+          throw new Error(error);
         }
-    
-    })
+      },
+    }),
   ],
-})
+});
