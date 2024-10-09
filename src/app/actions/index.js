032e07580566a4c40a9c9fd59a0160c1ce3ad2c9
Tashi Wangchuk <tashiwangchuk619@gmail.com>
2024-10-09 23:04:09 +0600

    add credential login

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -1,11 +1,24 @@
-'use server';
-import { signIn } from "@/auth";
-export async function doSocialLogin(formData){
-    const action = formData.get('action',{redirectTo:"/home"});
-    await signIn(action)
-
+"use server";
+import { signIn,signOut } from "@/auth";
+export async function doSocialLogin(formData) {
+  const action = formData.get("action", { redirectTo: "/home" });
+  await signIn(action,{redirectTo: "/home"});
 }
-
 export async function doLogout(){
-
+    await signOut({redirectTo:"/"})
 }
+
+// Credential login server action
+export async function doCredentialLogin(formData) {
+  try {
+    const response = await signIn("credentials", {
+        email: formData.get('email'),
+        password: formData.get('password'),
+        redirect:false
+    });
+    return response;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
