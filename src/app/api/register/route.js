161f62a663b1da59c7473aa222356e70e03a1de4
Tashi Wangchuk <tashiwangchuk619@gmail.com>
2024-10-09 23:07:06 +0600

    add register api

---
+++ src/app/api/register/route.js
@@ -0,0 +1,14 @@
+// import { NextResponse } from "next/server";
+// import {createUser} from '@/queries/users'
+// export const POST = async (request) => {
+//   const { name, email, password } = await request.json();
+//   console.log(name,email,password);
+
+//   //create a DB Connection
+//   //Encrypt the password
+//   //Form a DB Payload
+//   //Update the DB
+//   return new NextResponse("User has been created",{
+//     status: 201
+//   });
+// };
