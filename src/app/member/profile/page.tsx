31d7eae0f8617fcfbf295c24b01e7983f1c0fd2e
N0rphel <tnorphel09@gmail.com>
2024-11-04 23:49:24 +0600

    profileComponents

--- src/app/member/profile/page.tsx
+++ src/app/member/profile/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import BackgroundPicture from "@/components/Profile/BackgroundPicture";
+import ProfessionalDetailsCard from "@/components/Profile/ProfessionalDetailsCard";
+import ProfileCard from "@/components/Profile/ProfileCard";
+import React from "react";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+	return (
+		<div className="bg-background m-0">
+			<BackgroundPicture />
+			<div className="flex flex-col w-4/5 md:flex-row gap-6 p-6 bg-background min-h-screen">
+				<ProfileCard />
+				<ProfessionalDetailsCard />
+			</div>
+		</div>
+	);
+};
 
-export default page
+export default page;
